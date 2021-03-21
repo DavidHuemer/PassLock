575646f9cc06da2fb265abf70d74c9e26b13f604
DavidHuemer <92david.huemer@gmail.com>
2021-03-21 18:42:31 +0100

    Added change page functionality in the MainWindow

--- PassLock.GUI/ViewModels/MainViewModels/MainWindowViewModel.cs
+++ PassLock.GUI/ViewModels/MainViewModels/MainWindowViewModel.cs
@@ -1,8 +1,12 @@
-﻿using PassLock.GUI.ViewModels.Basics;
+﻿using MVVM.Tools;
+using PassLock.GUI.ViewModels.Basics;
 using PassLock.GUI.ViewModels.MainViewModels.Pages.Account;
 using PassLock.GUI.ViewModels.MainViewModels.Pages.Items;
 using PassLock.GUI.ViewModels.MainViewModels.Pages.Settings;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
 
 namespace PassLock.GUI.ViewModels.MainViewModels
 {
@@ -20,5 +24,13 @@ namespace PassLock.GUI.ViewModels.MainViewModels
 
         public List<PageViewModel> Pages { get; set; } = new List<PageViewModel>();
         public PageViewModel CurrentPage { get; set; }
+
+        public RelayCommand<PageViewModel> ChangePage => new RelayCommand<PageViewModel>(o => { DoChangePage(o); }, o => true);
+        
+        private void DoChangePage(PageViewModel o)
+        {
+            Console.WriteLine($"Change current page to {o.DisplayName}");
+            CurrentPage = Pages.FirstOrDefault(x => x == o);
+        }
     }
 }
