42c1d87435ff77d55916ec8bef6a98aa5510c221
DavidHuemer <92david.huemer@gmail.com>
2021-03-24 19:26:30 +0100

    Added RunModeHandler

---
+++ PassLock.Handler/Data/Diagnostics/RunModeHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace PassLock.Handler.Data.Diagnostics
+{
+    public static class RunModeHandler
+    {
+        /// <summary>
+        /// Returns if the application runs in debug mode or not
+        /// </summary>
+        /// <returns>If the application runs in debug mode</returns>
+        public static bool IsDebugMode()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
