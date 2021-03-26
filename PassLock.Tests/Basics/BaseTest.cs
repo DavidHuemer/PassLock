20e45291cfad4efb9266e95830e8ac785a8ee090
DavidHuemer <92david.huemer@gmail.com>
2021-03-26 23:01:43 +0100

    Added the default username and password to the BaseTest

--- PassLock.Tests/Basics/BaseTest.cs
+++ PassLock.Tests/Basics/BaseTest.cs
@@ -8,6 +8,7 @@ namespace PassLock.Tests.Basics
 {
     public class BaseTest
     {
-
+        public const string USERNAME = "example.email@gmail.com";
+        public const string PASSWORD = "password";
     }
 }
