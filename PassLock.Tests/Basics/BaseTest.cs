efd7217f20c49df2717be2b025074dc5d02c6eab
DavidHuemer <92david.huemer@gmail.com>
2021-03-26 23:03:35 +0100

    Removed unnessesary usings

--- PassLock.Tests/Basics/BaseTest.cs
+++ PassLock.Tests/Basics/BaseTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PassLock.Tests.Basics
+﻿namespace PassLock.Tests.Basics
 {
     public class BaseTest
     {
