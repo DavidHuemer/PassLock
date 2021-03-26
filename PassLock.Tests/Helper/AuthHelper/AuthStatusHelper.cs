cb0215c12908764dfa2ba2df15fe719fd59bcbf3
DavidHuemer <92david.huemer@gmail.com>
2021-03-26 23:02:44 +0100

    Added login helper for testing

---
+++ PassLock.Tests/Helper/AuthHelper/AuthStatusHelper.cs
@@ -0,0 +1,59 @@
+﻿using PassLock.Bitwarden.Data.Data.Authentication;
+using PassLock.Tests.Basics;
+using System;
+
+namespace PassLock.Tests.Helper.AuthHelper
+{
+    public class AuthStatusHelper
+    {
+        public const string LOGOUT_STATUS_OUTPUT = "{\"serverUrl\":\"https://bitwarden.com\",\"lastSync\":null,\"userEmail\":null,\"userId\":null,\"status\":\"unauthenticated\"}";
+        public const string LOCKET_STATUS_OUTPUT = "{\"serverUrl\":\"https://bitwarden.com\",\"lastSync\":\"2021-03-26T20:28:12.642Z\",\"userEmail\":\"" + BaseTest.USERNAME + "\",\"userId\":\"2cad48e5-a2h5-41a5-b00c-acbd01405774\",\"status\":\"locked\"}";
+        public const string UNLOCKED_STATUS_OUTPUT = "{\"serverUrl\":\"https://bitwarden.com\",\"lastSync\":\"2021-03-26T20:28:12.642Z\",\"userEmail\":\"" + BaseTest.USERNAME + "\",\"userId\":\"2cad48e5-a2h5-41a5-b00c-acbd01405774\",\"status\":\"unlocked\"}";
+
+        private const string BITWARDEN_URL = "https://bitwarden.com";
+        private const string USERNAME = BaseTest.USERNAME;
+        private const string USER_ID = "2cad48e5-a2h5-41a5-b00c-acbd01405774";
+
+        public static string GetAuthStatusOutputByBitwardenStatus(BitwardenStatus status)
+        {
+            switch (status)
+            {
+                case BitwardenStatus.Logout:
+                    return LOGOUT_STATUS_OUTPUT;
+                case BitwardenStatus.Locked:
+                    return LOCKET_STATUS_OUTPUT;
+                case BitwardenStatus.Unlocked:
+                    return UNLOCKED_STATUS_OUTPUT;
+                default:
+                    throw new ArgumentException($"The status {status} does not exist");
+            }
+        }
+
+        public static BitwardenAuthStatus GetAuthStatusByBitwardenStatus(BitwardenStatus status)
+        {
+            switch (status)
+            {
+                case BitwardenStatus.Logout:
+                    return CreateBitwardenAuthStatus(null, null, null, BitwardenStatus.Logout);
+                case BitwardenStatus.Locked:
+                    return CreateBitwardenAuthStatus(DateTime.Now, USERNAME, USER_ID, BitwardenStatus.Locked);
+                case BitwardenStatus.Unlocked:
+                    return CreateBitwardenAuthStatus(DateTime.Now, USERNAME, USER_ID, BitwardenStatus.Unlocked);
+                default:
+                    throw new ArgumentException($"The status {status} does not exist");
+            }
+        }
+
+        public static BitwardenAuthStatus CreateBitwardenAuthStatus(DateTime? lastSync, string userEmail, string userId, BitwardenStatus status)
+        {
+            return new BitwardenAuthStatus
+            {
+                ServerUrl = BITWARDEN_URL,
+                LastSync = lastSync,
+                UserEmail = userEmail,
+                UserId = userId,
+                Status = status
+            };
+        }
+    }
+}
---
+++ PassLock.Tests/Helper/AuthHelper/LoginHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassLock.Tests.Helper.AuthHelper
+{
+    public static class LoginHelper
+    {
+        public const string CORRECT_LOGIN = "You are logged in!\r\n\r\nTo unlock your vault, set your session key to the `BW_SESSION` environment variable. ex:\r\n$ export BW_SESSION=\"AVi2U8sypZ/K+NCbfS5O8hpPxApqkpuCikBwNSJQ2kxQsBV8qwDyAU/Ty/sOIr1IlycTvekjWBtPtfypMGITaw==\"\r\n> $env:BW_SESSION=\"AVi2U8sypZ/K+NCbfS5O8hpPxApqkpuCikBwNSJQ2kxQsBV8qwDyAU/Ty/sOIr1IlycTvekjWBtPtfypMGITaw==\"\r\n\r\nYou can also pass the session key to any command with the `--session` option. ex:\r\n$ bw list items --session AVi2U8sypZ/K+NCbfS5O8hpPxApqkpuCikBwNSJQ2kxQsBV8qwDyAU/Ty/sOIr1IlycTvekjWBtPtfypMGITaw==";
+        public const string SECCOND_LOGIN = "You are already logged in as example.email@gmail.com";
+        public const string INVALID_LOGIN = "Username or password is incorrect. Try again.";
+
+        public const string CORRECT_UNLOCK = "Your vault is now unlocked!\r\n\r\nTo unlock your vault, set your session key to the `BW_SESSION` environment variable. ex:\r\n$ export BW_SESSION=\"p4Qv+iKLipazwetwW288Zk7UT8AsTQIcgRAroouQILltdz2WNZkyh1zv8uID84dylXQyquaurutvyzg5utQDaA==\"\r\n> $env:BW_SESSION=\"p4Qv+iKLipazwetwW288Zk7UT8AsTQIcgRAroouQILltdz2WNZkyh1zv8uID84dylXQyquaurutvyzg5utQDaA==\"\r\n\r\nYou can also pass the session key to any command with the `--session` option. ex:\r\n$ bw list items --session p4Qv+iKLipazwetwW288Zk7UT8AsTQIcgRAroouQILltdz2WNZkyh1zv8uID84dylXQyquaurutvyzg5utQDaA==";
+        public const string INVALID_UNLOCK = "Invalid master password";
+    }
+}
