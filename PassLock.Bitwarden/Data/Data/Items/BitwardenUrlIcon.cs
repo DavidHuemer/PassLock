431d7f0647c1e228df4c8617cd519e3a0d8a0038
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 16:16:49 +0200

    Removed unused Items

--- PassLock.Bitwarden/Data/Data/Items/BitwardenUrlIcon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PassLock.Bitwarden.Data.Data.Items
-{
-    public class BitwardenUrlIcon
-    {
-        public string Url { get; set; }
-        public string IconName { get; set; }
-    }
-}
