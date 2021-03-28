396e13426bf3b5b017239bc4a328abbcdeba34e5
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 17:18:56 +0200

    Added BitwardenFoldersService

---
+++ PassLock.Bitwarden/Services/ObjectServices/BitwardenFoldersService.cs
@@ -0,0 +1,18 @@
+﻿using PassLock.Bitwarden.Handler.BitwardenCommandLine;
+using PassLock.Bitwarden.Services.Interfaces;
+using PassLock.Handler.CommandLineHandler.Data;
+using System.Threading.Tasks;
+
+namespace PassLock.Bitwarden.Services.ObjectServices
+{
+    public class BitwardenFoldersService : IBitwardenObjectsService
+    {
+        public const string BW_LOAD_FOLDERS_COMMAND = "bw list folders";
+
+        public async Task<CommandResult> LoadItems()
+        {
+            var result = await BitwardenCLIHandler.RunCommandAsync(BW_LOAD_FOLDERS_COMMAND);
+            return result;
+        }
+    }
+}
