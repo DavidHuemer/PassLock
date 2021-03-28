7b5bf6d39ff1d581b59049f7d2fbb8ee143d54ae
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 14:41:01 +0200

    Added BitwardenObject

---
+++ PassLock.Bitwarden/Data/Data/Objects/BitwardenObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace PassLock.Bitwarden.Data.Data.Objects
+{
+    /// <summary>
+    /// Base class for all BitwardenObjects
+    /// </summary>
+    public abstract class BitwardenObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+    }
+}
