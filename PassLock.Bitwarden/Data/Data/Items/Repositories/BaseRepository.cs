f986c25a84cd5fe2f3c0558067d529c89444c8db
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 12:20:16 +0200

    Added BaseRepository

---
+++ PassLock.Bitwarden/Data/Data/Items/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace PassLock.Bitwarden.Data.Data.Items.Repositories
+{
+    /// <summary>
+    /// Base repository for all repositories
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseRepository<T> : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The stored items
+        /// </summary>
+        public static ObservableCollection<T> Items { get; protected set; } = new ObservableCollection<T>();
+
+        /// <summary>
+        /// Updates the items
+        /// </summary>
+        public virtual void UpdateItems()
+        {
+            Console.WriteLine($"BaseRepository update items");
+            Items.Clear();
+            var items = LoadItems();
+            foreach (var item in items)
+            {
+                Items.Add(item);
+            }
+            Console.WriteLine($"BaseRepository updated items");
+        }
+
+        /// <summary>
+        /// Loads the items from the source
+        /// </summary>
+        /// <returns>The loaded items</returns>
+        protected abstract List<T> LoadItems();
+    }
+}
