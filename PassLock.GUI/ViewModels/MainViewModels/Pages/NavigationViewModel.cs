f36bec4270a5ec1be83f46b15e1aef50d7340324
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 23:29:30 +0200

    Added NavigationViewModel

---
+++ PassLock.GUI/ViewModels/MainViewModels/Pages/NavigationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PassLock.GUI.ViewModels.Basics;
+
+namespace PassLock.GUI.ViewModels.MainViewModels.Pages
+{
+    /// <summary>
+    /// Handles the navigation on the MainWindow
+    /// </summary>
+    public class NavigationViewModel : BaseViewModel
+    {
+    }
+}
