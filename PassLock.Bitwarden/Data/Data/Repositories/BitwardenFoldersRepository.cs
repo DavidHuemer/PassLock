e443fbb5f1d36954634161ca47619e6446a03a2e
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 17:11:20 +0200

    Added BitwardenFoldersRepository and tests

---
+++ PassLock.Bitwarden/Data/Data/Repositories/BitwardenFoldersRepository.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using PassLock.Bitwarden.Data.Data.Objects.Folders;
+using PassLock.Bitwarden.Data.DTOs.Objects.Folders;
+using PassLock.Handler.CommandLineHandler.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassLock.Bitwarden.Data.Data.Repositories
+{
+    public class BitwardenFoldersRepository : BaseRepository<BitwardenFolder>
+    {
+        #region Singleton
+
+        private static BitwardenFoldersRepository instance;
+        public static BitwardenFoldersRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BitwardenFoldersRepository();
+                return instance;
+            }
+        }
+
+        private BitwardenFoldersRepository()
+        {
+
+        }
+
+        #endregion
+
+        public override async Task<List<BitwardenFolder>> LoadItems()
+        {
+            var result = await service.LoadItems();
+            if (result.Status != CommandStatus.Success)
+                throw new ArgumentException($"Error loading folders: {result.Output}");
+
+            var dtos = JsonConvert.DeserializeObject<List<BitwardenFolderDTO>>(result.Output);
+            return dtos.Select(x => x.ConvertBack()).ToList();
+        }
+    }
+}
---
+++ PassLock.Tests/Bitwarden/Data/Repositories/BitwardenFoldersRepository_Tests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using PassLock.Bitwarden.Data.Data.Repositories;
+using PassLock.Bitwarden.Services.Interfaces;
+using PassLock.Tests.Basics;
+using PassLock.Tests.Helper.Objects.Folders;
+using Xunit;
+
+namespace PassLock.Tests.Bitwarden.Data.Repositories
+{
+    public class BitwardenFoldersRepository_Tests : MoqTest
+    {
+        private Mock<IBitwardenObjectsService> mock;
+
+        public BitwardenFoldersRepository_Tests()
+        {
+            mock = new Mock<IBitwardenObjectsService>();
+        }
+
+        #region LoadItems Count Test
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void LoadItems_CountTest(int count)
+        {
+            var jsonStr = FolderDTOsHelper.CreateFolderDTOJsonList(count);
+            mock.Reset();
+            var t = CreateCommandResultTaskFromOutput(jsonStr);
+            mock.Setup(service => service.LoadItems()).Returns(t);
+            BitwardenFoldersRepository.Instance.Init(mock.Object);
+            var items = await BitwardenFoldersRepository.Instance.LoadItems();
+            Assert.Equal(count, items.Count);
+        }
+
+        #endregion
+
+        #region UpdateItems Count Test
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void UpdateItems_CountTest(int count)
+        {
+            var jsonStr = FolderDTOsHelper.CreateFolderDTOJsonList(count);
+            mock.Reset();
+            var t = CreateCommandResultTaskFromOutput(jsonStr);
+            mock.Setup(service => service.LoadItems()).Returns(t);
+            BitwardenFoldersRepository.Instance.Init(mock.Object);
+            await BitwardenFoldersRepository.Instance.UpdateItems();
+            Assert.Equal(count, BitwardenFoldersRepository.Instance.Items.Count);
+        }
+
+        #endregion
+    }
+}
