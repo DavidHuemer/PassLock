e4fae17efe4d93e55025979ad85337c1f69b4f7a
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 17:56:27 +0200

    Added BitwardenItemsRepository and tests for it

---
+++ PassLock.Bitwarden/Data/Data/Repositories/BitwardenItemsRepository.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using PassLock.Bitwarden.Data.Data.Objects.Items;
+using PassLock.Bitwarden.Data.DTOs.Objects.Items;
+using PassLock.Handler.CommandLineHandler.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassLock.Bitwarden.Data.Data.Repositories
+{
+    /// <summary>
+    /// Represents the bitwarden vault
+    /// </summary>
+    public class BitwardenItemsRepository : BaseRepository<BitwardenItem>
+    {
+        #region Singleton
+
+        private static BitwardenItemsRepository instance;
+        public static BitwardenItemsRepository Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BitwardenItemsRepository();
+                return instance;
+            }
+        }
+
+        private BitwardenItemsRepository()
+        {
+
+        }
+
+        #endregion
+
+        public override async Task<List<BitwardenItem>> LoadItems()
+        {
+            var result = await service.LoadItems();
+            if (result.Status != CommandStatus.Success)
+                throw new ArgumentException($"Error loading folders: {result.Output}");
+
+            var dtos = JsonConvert.DeserializeObject<List<BitwardenItemDTO>>(result.Output);
+            return dtos.Select(x => x.ConvertBack()).ToList();
+        }
+
+        public async Task UpdateItems(bool loadIcons = true)
+        {
+            await base.UpdateItems();
+            if (loadIcons)
+            {
+                Console.WriteLine("Icons");
+            }
+        }
+    }
+}
---
+++ PassLock.Tests/Bitwarden/Data/Repositories/BitwardenItemsRepository_Tests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using PassLock.Bitwarden.Data.Data.Repositories;
+using PassLock.Bitwarden.Services.Interfaces;
+using PassLock.Tests.Basics;
+using PassLock.Tests.Helper.ItemHelpers;
+using Xunit;
+
+namespace PassLock.Tests.Bitwarden.Data.Repositories
+{
+    public class BitwardenItemsRepository_Tests : MoqTest
+    {
+        private Mock<IBitwardenObjectsService> mock;
+
+        public BitwardenItemsRepository_Tests()
+        {
+            mock = new Mock<IBitwardenObjectsService>();
+        }
+
+        #region LoadItems Count Test
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void LoadItems_CountTest(int count)
+        {
+            var jsonStr = ItemsJsonDTOsHelper.CreateItemsDTOJsonList(count);
+            mock.Reset();
+            var t = CreateCommandResultTaskFromOutput(jsonStr);
+            mock.Setup(service => service.LoadItems()).Returns(t);
+            BitwardenItemsRepository.Instance.Init(mock.Object);
+            var items = await BitwardenItemsRepository.Instance.LoadItems();
+            Assert.Equal(count, items.Count);
+        }
+
+        #endregion
+
+        #region UpdateItems Count Test
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void UpdateItems_CountTest(int count)
+        {
+            var jsonStr = ItemsJsonDTOsHelper.CreateItemsDTOJsonList(count);
+            mock.Reset();
+            var t = CreateCommandResultTaskFromOutput(jsonStr);
+            mock.Setup(service => service.LoadItems()).Returns(t);
+            BitwardenItemsRepository.Instance.Init(mock.Object);
+            await BitwardenItemsRepository.Instance.UpdateItems(false);
+            Assert.Equal(count, BitwardenItemsRepository.Instance.Items.Count);
+        }
+
+        #endregion
+    }
+}
