ae57b3a9c077c6a36ea1dc24088a1b7518d7b310
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 17:20:31 +0200

    Added BitwardenFolderDTO

---
+++ PassLock.Bitwarden/Data/DTOs/Objects/Folders/BitwardenFolderDTO.cs
@@ -0,0 +1,16 @@
+﻿using PassLock.Bitwarden.Data.Data.Objects.Folders;
+
+namespace PassLock.Bitwarden.Data.DTOs.Objects.Folders
+{
+    public class BitwardenFolderDTO : BitwardenObjectDTO<BitwardenFolder>
+    {
+        public override BitwardenFolder ConvertBack()
+        {
+            return new BitwardenFolder
+            {
+                Id = Id,
+                Name = Name
+            };
+        }
+    }
+}
