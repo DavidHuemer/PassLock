f787806b290d30c1cc36afeca9a2f4a9c43dad50
DavidHuemer <92david.huemer@gmail.com>
2021-03-28 17:12:05 +0200

    Added tests for the folderDTO

---
+++ PassLock.Tests/Bitwarden/DTOs/Objects/Folders/BitwardenFolderDTO_Tests.cs
@@ -0,0 +1,18 @@
+﻿using PassLock.Tests.Basics;
+using PassLock.Tests.Helper.Objects.Folders;
+using Xunit;
+
+namespace PassLock.Tests.Bitwarden.DTOs.Objects.Folders
+{
+    public class BitwardenFolderDTO_Tests : BaseTest
+    {
+        [Fact]
+        public void FolderDTO_Test()
+        {
+            var dto = FolderDTOsHelper.CreateFolderDTO();
+            var folder = dto.ConvertBack();
+            Assert.Equal(FolderDTOsHelper.ID, folder.Id);
+            Assert.Equal(FolderDTOsHelper.NAME, folder.Name);
+        }
+    }
+}
