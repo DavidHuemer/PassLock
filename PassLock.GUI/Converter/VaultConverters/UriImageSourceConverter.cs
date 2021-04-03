140bdff00004b0f2ee3b870bd45eb8e7be35611e
DavidHuemer <92david.huemer@gmail.com>
2021-04-03 21:37:12 +0200

    Added UriImageSourceConverter

---
+++ PassLock.GUI/Converter/VaultConverters/UriImageSourceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PassLock.GUI.Converter.VaultConverters
+{
+    public class UriImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string uriStr = (string)value;
+            if (uriStr == null)
+            {
+                uriStr = "pack://application:,,,/PassLock.GUI;component/Resources/Icons/default_favicon.ico";
+                uriStr = @"https://github.com/favicon.ico";
+            }
+
+            return uriStr;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
