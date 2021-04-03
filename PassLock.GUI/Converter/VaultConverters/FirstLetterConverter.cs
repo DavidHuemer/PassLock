9303edd9e6e19be8523ea9702ffa091428d05769
DavidHuemer <92david.huemer@gmail.com>
2021-04-03 21:28:03 +0200

    Added FirstLetter and SearchTextConverter

---
+++ PassLock.GUI/Converter/VaultConverters/FirstLetterConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PassLock.GUI.Converter.VaultConverters
+{
+    public class FirstLetterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            if (s != null && s.Length > 0)
+                return s.Substring(0, 1);
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PassLock.GUI/Converter/VaultConverters/SearchTextConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PassLock.GUI.Converter.VaultConverters
+{
+    public class SearchTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"Search in {value}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
