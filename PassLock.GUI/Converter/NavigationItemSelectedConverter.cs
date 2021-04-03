ce21f417d1c1668d632ef4b08e5e47b088c841d0
DavidHuemer <92david.huemer@gmail.com>
2021-04-03 16:58:57 +0200

    Added NavigationItemSelectedConverter

---
+++ PassLock.GUI/Converter/NavigationItemSelectedConverter.cs
@@ -0,0 +1,24 @@
+﻿using PassLock.GUI.Controls.Custom.Buttons;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PassLock.GUI.Converter
+{
+    public class NavigationItemSelectedConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value[0] == null || value[1] == null)
+                return false;
+
+            var navButton = value[1] as NavigationButton;
+            return value[0] == navButton.DataContext;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
