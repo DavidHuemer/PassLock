ede5ce351b25c7aff05001b728d5f910b79d8f8e
DavidHuemer <92david.huemer@gmail.com>
2021-04-03 16:59:09 +0200

    Added ResourceHandler

---
+++ PassLock.GUI/Resources/ResourceHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PassLock.GUI.Resources
+{
+    public static class ResourceHandler
+    {
+        public static T Get<T>(string resourceName) where T : class
+        {
+            return (T)Application.Current.TryFindResource(resourceName);
+        }
+    }
+}
