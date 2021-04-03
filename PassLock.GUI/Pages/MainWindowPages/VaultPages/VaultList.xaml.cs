367d7484c7d62809c4f0df948ed9cef175a39f29
DavidHuemer <92david.huemer@gmail.com>
2021-04-03 21:37:29 +0200

    Added VaultPage and VaultList

---
+++ PassLock.GUI/Pages/MainWindowPages/VaultPages/VaultList.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PassLock.GUI.Pages.MainWindowPages.VaultPages
+{
+    /// <summary>
+    /// Interaction logic for ItemsListUC.xaml
+    /// </summary>
+    public partial class VaultList : UserControl
+    {
+        public VaultList()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PassLock.GUI/Pages/MainWindowPages/VaultPages/VaultPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PassLock.GUI.Pages.MainWindowPages.VaultPages
+{
+    /// <summary>
+    /// Interaction logic for VaultPage.xaml
+    /// </summary>
+    public partial class VaultPage : UserControl
+    {
+        public VaultPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
