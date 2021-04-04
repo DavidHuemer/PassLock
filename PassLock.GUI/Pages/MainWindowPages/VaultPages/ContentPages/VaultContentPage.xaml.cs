a242a3d345d95b9159001e942e1f78479b36ec6e
DavidHuemer <92david.huemer@gmail.com>
2021-04-04 13:46:54 +0200

    Added VaultContentPage

---
+++ PassLock.GUI/Pages/MainWindowPages/VaultPages/ContentPages/VaultContentPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PassLock.GUI.Pages.MainWindowPages.VaultPages.ContentPages
+{
+    /// <summary>
+    /// Interaction logic for VaultContentPage.xaml
+    /// </summary>
+    public partial class VaultContentPage : UserControl
+    {
+        public VaultContentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
