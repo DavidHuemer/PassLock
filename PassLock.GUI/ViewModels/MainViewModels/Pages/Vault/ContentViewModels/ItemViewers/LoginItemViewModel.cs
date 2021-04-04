5e7cb4e8b01ebcc10fe620e1ae1766be75b4b546
DavidHuemer <92david.huemer@gmail.com>
2021-04-04 13:49:55 +0200

    Added LoginItemViewModel

---
+++ PassLock.GUI/ViewModels/MainViewModels/Pages/Vault/ContentViewModels/ItemViewers/LoginItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.Basics;
+
+namespace PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.ItemViewers
+{
+    public class LoginItemViewModel : BaseItemViewerViewModel
+    {
+
+    }
+}
