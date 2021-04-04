3bfccd1dc0a88ba7a343bb681a04a377e45ea460
DavidHuemer <92david.huemer@gmail.com>
2021-04-04 13:48:57 +0200

    Added BaseItemViewerViewModel and BaseVaultContentViewModel

---
+++ PassLock.GUI/ViewModels/MainViewModels/Pages/Vault/ContentViewModels/Basics/BaseItemViewerViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.Basics
+{
+    /// <summary>
+    /// Base class for all item viewers
+    /// </summary>
+    public abstract class BaseItemViewerViewModel : BaseVaultContentViewModel
+    {
+
+    }
+}
---
+++ PassLock.GUI/ViewModels/MainViewModels/Pages/Vault/ContentViewModels/Basics/BaseVaultContentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PassLock.GUI.ViewModels.Basics;
+
+namespace PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.Basics
+{
+    /// <summary>
+    /// Base class for everything that is inside the content of the vault page
+    /// </summary>
+    public abstract class BaseVaultContentViewModel : BaseViewModel
+    {
+
+    }
+}
