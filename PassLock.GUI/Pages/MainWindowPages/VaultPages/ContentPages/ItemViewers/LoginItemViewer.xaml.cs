f9e31cf02a0c92a1551472eac44704c4e83c8e63
DavidHuemer <92david.huemer@gmail.com>
2021-04-04 13:44:33 +0200

    Added LoginItemViewer

---
+++ PassLock.GUI/Pages/MainWindowPages/VaultPages/ContentPages/ItemViewers/LoginItemViewer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PassLock.GUI.Pages.MainWindowPages.VaultPages.ContentPages.ItemViewers
+{
+    /// <summary>
+    /// Interaction logic for LoginItemViewer.xaml
+    /// </summary>
+    public partial class LoginItemViewer : UserControl
+    {
+        public LoginItemViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
