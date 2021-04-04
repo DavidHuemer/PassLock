f3288900dfb42d8b900f535cee3d8657a8793337
DavidHuemer <92david.huemer@gmail.com>
2021-04-04 13:49:10 +0200

    Added BitwardenItemCreationViewModel

---
+++ PassLock.GUI/ViewModels/MainViewModels/Pages/Vault/ContentViewModels/CreationViewModels/BitwardenItemCreationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PassLock.GUI.ViewModels.Basics;
+using PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.Basics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassLock.GUI.ViewModels.MainViewModels.Pages.Vault.ContentViewModels.CreationViewModels
+{
+    public class BitwardenItemCreationViewModel : BaseVaultContentViewModel
+    {
+        public BitwardenItemCreationViewModel()
+        {
+
+        }
+    }
+}
